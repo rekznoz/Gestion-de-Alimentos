6737f7759016bdb155bc7f8a56fc6aefeec60978
RekZNoZ <rafikicroz@hotmail.com>
2024-12-12 00:12:32 +0100

    Update UbicacionService.java

--- src/main/java/daw2a/gestionalimentos/services/UbicacionService.java
+++ src/main/java/daw2a/gestionalimentos/services/UbicacionService.java
@@ -6,7 +6,6 @@ import daw2a.gestionalimentos.dto.ubicacion.UbicacionUpdateDTO;
 import daw2a.gestionalimentos.entities.Ubicacion;
 import daw2a.gestionalimentos.enums.EnumUbicacion;
 import daw2a.gestionalimentos.repositories.UbicacionRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
