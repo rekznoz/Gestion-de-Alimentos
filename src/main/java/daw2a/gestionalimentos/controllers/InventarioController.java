d8e4873cc9522860d12c1a6962f381bb779fe695
RekZNoZ <rafikicroz@hotmail.com>
2024-12-12 17:17:12 +0100

    Desativada Seguridad

--- src/main/java/daw2a/gestionalimentos/controllers/InventarioController.java
+++ src/main/java/daw2a/gestionalimentos/controllers/InventarioController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/inventario-usuario")
+@RequestMapping("/inventarios")
 public class InventarioController {
 
     private final InventarioService inventarioService;
