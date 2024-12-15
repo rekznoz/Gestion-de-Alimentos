f4e3ceaa442b4109a3ebba2858f085acebe00c18
RekZNoZ <rafikicroz@hotmail.com>
2024-12-15 01:53:36 +0100

    Update InventarioRepository.java

--- src/main/java/daw2a/gestionalimentos/repositories/InventarioRepository.java
+++ src/main/java/daw2a/gestionalimentos/repositories/InventarioRepository.java
@@ -1,15 +1,14 @@
 package daw2a.gestionalimentos.repositories;
 
 import daw2a.gestionalimentos.entities.Inventario;
-import daw2a.gestionalimentos.entities.Ubicacion;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
+/**
+ * Repositorio de la entidad Inventario
+ */
 @Repository
 public interface InventarioRepository extends JpaRepository<Inventario, Long> {
 
