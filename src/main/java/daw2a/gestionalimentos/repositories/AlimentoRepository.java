b3881f25f6da2b7618a6ce4ad0d9791c659eac62
RekZNoZ <rafikicroz@hotmail.com>
2024-12-15 01:53:32 +0100

    Update AlimentoRepository.java

--- src/main/java/daw2a/gestionalimentos/repositories/AlimentoRepository.java
+++ src/main/java/daw2a/gestionalimentos/repositories/AlimentoRepository.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 
+/**
+ * Repositorio de la entidad Alimento
+ */
 @Repository
 public interface AlimentoRepository  extends JpaRepository<Alimento,Long> {
 
