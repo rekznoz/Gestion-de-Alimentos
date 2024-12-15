bd2fda1dde83ad9c6fd9e1be5cf5d388434f48f0
RekZNoZ <rafikicroz@hotmail.com>
2024-12-15 01:53:40 +0100

    Update UsuarioRepository.java

--- src/main/java/daw2a/gestionalimentos/repositories/UsuarioRepository.java
+++ src/main/java/daw2a/gestionalimentos/repositories/UsuarioRepository.java
@@ -6,6 +6,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repositorio de la entidad Usuario
+ */
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
